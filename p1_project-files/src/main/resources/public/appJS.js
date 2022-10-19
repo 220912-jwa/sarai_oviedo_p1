d99583c90c569a7199c787ab885ce721e348be0c
lemonadedance <sopphey@gmail.com>
2022-10-19 16:59:12 -0500

    end-of-day changes. added more functionality to displaying employee requests

--- p1_project-files/src/main/resources/public/appJS.js
+++ p1_project-files/src/main/resources/public/appJS.js
@@ -13,7 +13,9 @@ async function userLogin() {
     credentialsJSON = JSON.stringify(credentials);
 
         let res = await fetch(
+
         `${baseURL}/authenticate`,
+
         {
             method: 'POST',
             header: {'Content-Type': 'application/json'},
@@ -22,73 +24,50 @@ async function userLogin() {
     );
 
     if (res.status===200){
+
         let loggedInUser = await res.json();
+        
         sessionStorage.setItem('userName', loggedInUser.userFName);
         sessionStorage.setItem('userRoleID', loggedInUser.userRoleID);
+
         document.location.assign("./home.html");
-    }
-
-}
-
-function loadPageForUserWithSessionStorage() {
-
-    let name = sessionStorage.getItem("userName");
-    let role = sessionStorage.getItem("userRoleID");
-    let staffID = sessonStorage.getItem("staffID");
-
-    document.getElementById("homeHeader").innerHTML = `Welcome Back, ${name}.`;
-
-    if (role == 2){
-
-        userIsEmployeeDisplay();
-        
-    } else if (role == 1){
-
-        userIsFinanceManagerDisplay();
 
     }
 }
 
-async function userIsEmployeeDisplay(){
-    
-    allByStaffID();
-
-}
-
-async function userIsFinanceManagerDisplay(){
-    
-    allRequests();
-    
-}
+let name = sessionStorage.getItem("userName");
+let role = sessionStorage.getItem("userRoleID");
+let staffID = sessonStorage.getItem("staffID");
 
 async function allByStaffID(){
-    //load all requests by staffID:
+    //load all requests by staffID functionality
         
-    //make a call to the backend:
+    //make a call to the backend
     
-    //define reference variable
+        //define reference variable
 
-    let allAllByStaffID = document.getElementById('staffID').value;
+    let allByStaffID = document.getElementById('staffID').value;
     
-    //GET request using fetch with async/await
+        //send GET request using fetchAPI with async/await
 
-    let resAllByStaffID = await fetch();
+    let resAllByStaffID = await fetch(`${baseURL}/all-requests/${allByStaffID}`);
 
-    if (resAllByStaffID.status ===200){
-        let resAllByStaffIDJSON = await resAllByStaffID.json(`${baseURL}/all-requests/${all}`);
+    if (resAllByStaffID.status === 200){
+        
+        let resAllByStaffIDArray = await resAllByStaffID.json(); //returns Array
+
+        //select existing table from DOM
+        let requestsTableBody = document.getElementById('homeTableHeader');
+    
+        //create a new tr
+                
+        //create a td for each property we want displayed in the table       
+                      
+        //add the newly created tds to the newly created tr using while-loop
+        
+        //add the tr to the existing table from DOM
+    
     }
-
-    //select existing table from DOM
-    let requestsTableBody = document.getElementById('homeTableHeader');
-    
-    //create a new tr
-    //let requetsTR
-    
-    //create a td for each property we want displayed in the table
-    
-    //add the newly created tds to the newly created tr
-    
-    //add the tr to the existing table from DOM
 }
 
 async function allRequests(){
@@ -98,4 +77,19 @@ async function allRequests(){
     //create a td for each property we want displayed in the table
     //add the newly created tds to the newly created tr
     //add the tr to the existing table from DOM
+}
+
+function loadPageForUserWithSessionStorage() {
+
+    document.getElementById("homeHeader").innerHTML = `Welcome Back, ${name}.`;
+
+    if (role == 2){
+
+        allByStaffID();
+        
+    } else if (role == 1){
+
+        allRequests();
+
+    }
 }
